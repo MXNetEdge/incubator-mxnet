8afda7b18ee90e04235edee53fc2963bd402f45c
Hetzel <ahhetzel@amazon.com>
2018-04-11 17:42:51 +0200

    gpu version of beam search update

---
+++ src/operator/tensor/sockeye_beam_search.cu
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * Copyright (c) 2018 by Contributors
+ * \file sockeye_beam_search.cc
+ * \brief GPU-Operators for Sockeye beam search.
+ */
+#include "./sockeye_beam_search.h"
+
+namespace mxnet {
+namespace op {
+
+
+NNVM_REGISTER_OP(_sockeye_beam_search)
+.set_attr<FCompute>("FCompute<gpu>", SockeyeBeamSearchForward<gpu>);
+
+}  // namespace op
+}  // namespace mxnet
